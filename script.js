841f2fd49a3388e3c27401090c625eaecad77e8c
cheryy3007 <zulfiyagazna@gmail.com>
2025-02-19 22:30:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+const containerCards = document.getElementById("container-cards");
+const daynight = document.getElementById("daynight");
+const searchInput = document.getElementById("search"); // Убедись, что input имеет id="search"
+const selectocean = document.getElementById("selectocean");
+let countries = []; // Сюда сохраним все данные из API
+
+async function fetchAPI() {
+    try {
+        const res = await fetch("https://restcountries.com/v3.1/all");
+        const data = await res.json();
+        countries = data; // Сохраняем массив стран
+        console.log(countries);
+        generator(countries); // Отрисовываем карточки
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fetchAPI();
+
+function generator(products) { 
+    containerCards.innerHTML = ''; // Очищаем контейнер перед новой генерацией
+    products.forEach(element => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <img src="${element.flags.png}" alt="flag">
+            <h3>${element.name.common}</h3>
+        `;
+        containerCards.appendChild(card);
+    });
+}
+
+searchInput.addEventListener("input", async () => {
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    const data = await res.json();
+    const searchQuery = searchInput.value.toLowerCase();
+    const filteredCountries = countries.filter(country =>
+        country.name.common.toLowerCase().includes(searchQuery)
+    );
+    generator(filteredCountries);
+});
+selectocean.addEventListener("change", async () => {
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    const data = await res.json();
+    const regionQuery = selectocean.value.toLowerCase();
+    const filteredCountries = countries.filter(country =>
+        country.region.toLowerCase().includes(regionQuery)
+    );
+    generator(filteredCountries);
+});
+// Загружаем тему из localStorage
+if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("dark-mode");
+}
+
+daynight.addEventListener("click", () => {
+    if (document.body.classList.toggle("light-mode")) {
+        daynight.textContent = " Dark mode🌙";
+    } else {
+        daynight.textContent = "Light mode☀️";
+    }
+    document.body.classList.toggle("light-mode");
+    localStorage.setItem("theme", document.body.classList.contains("dark-mode") ? "dark" : "light");
+});
+
+
+
